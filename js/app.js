063a5e7bdb52f00c3a4de9c7b926c4041ded89aa
xbayazid <bayazid9006@gmail.com>
2022-09-17 11:33:22 +0600

    image, product & total solved

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
@@ -74,9 +74,9 @@ const getInputValue = (id) => {
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total;
 };
 
 // set innerText function
